8a853aa668255f57ee0fdddd7889e0338b0bea59
Patrik Miculka <MiPatrik@gmail.com>
2018-12-11 20:21:50 +0100

    init commit

---
+++ src/main/java/guru/springframework/jokesapp/services/GenerateService.java
@@ -0,0 +1,11 @@
+package guru.springframework.jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public interface GenerateService  {
+
+
+    public String getRandomQuote() {
+        return super.getRandomQuote();
+    }
+}
